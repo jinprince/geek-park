c289661b7df594833a7ef3168e097dd0f092f721
mx-jin <2457376055@qq.com>
2022-08-12 11:23:02 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+// import './App.scss'
+const Login = React.lazy(() => import('@/pages/Login'))
+const Home = React.lazy(() => import('@/pages/Home'))
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="app">
+        {/* <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link> */}
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Redirect exact from="/" to="/home"></Redirect>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
